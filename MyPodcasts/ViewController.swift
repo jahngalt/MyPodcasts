fb17ab229cb0e68fd74f7e3218483c0296b80825
Oleg Kudimov <ovkudimov@gmail.com>
2020-06-12 10:29:35 +0300

    Initial Commit

---
+++ MyPodcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPodcasts
+//
+//  Created by Oleg Kudimov on 6/12/20.
+//  Copyright © 2020 Oleg Kudimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
